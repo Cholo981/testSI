c9cb2ab6e43d35423b3ebc2d224dc69e0c92ab7a
Cholo981 <shaq981@yahoo.it>
2021-06-01 11:35:14 +0200

    init mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace testSI.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
